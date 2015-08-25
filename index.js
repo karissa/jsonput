c61c47df59f4af0ce85232bb151fdadd5891c310
Karissa McKelvey <krmckelv@gmail.com>
2015-08-25 23:23:27 +0100

    1.0.0

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+/*
+  jsonput venues.json "Bloomsbury Coffee House Wireless":"4e95b2e2b8f73812d09ba1f0"
+*/
+var fs = require('fs')
+var concat = require('concat-stream')
+var file = process.argv[2]
+var keys = process.argv[3]
+
+if (file === '-') {
+  var input = process.stdin
+  var writer = process.stdout
+} else {
+  var input = fs.createReadStream(file)
+  var writer = fs.createWriteStream(file)
+}
+
+input.pipe(concat(function (buf) {
+  try {
+    var json = JSON.parse(buf || "{}")
+  } catch (e) {
+    console.error('Invalid JSON')
+    console.trace(e)
+    process.exit(1)
+  }
+
+  var parts = keys.split(':')
+  json[parts[0]] = JSON.parse(parts[1])
+
+  writer.write(JSON.stringify(json))
+}))
+
+function buildValue (parts) {
+  if (!parts[1]) return parts[0]
+  if (parts[1].indexOf(':') > -1) return parts[1]
+  return parts[0] + buildValue(parts.splice(1))
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,30 @@
-#jsonput
-[![NPM](https://nodei.co/npm/jsonput.png)](https://nodei.co/npm/jsonput/)
+# jsonput
+
+Put json key/value pairs into a file or json stream from the command line
+
+#### install
+
+```
+npm install -g jsonput
+```
+
+#### example
+
+```
+$ cat test.json
+{"hello": "world"}
+$ jsonput test.json hello:mars
+$ cat test.json
+{"hello":"mars"}
+$ cat test.json | jsonput - hello:verden > test.json
+$ cat test.json
+{"hello" "verden"}
+```
+
+#### todo
+```
+$ jsonput test.json hello:{places:[mars,world]}
+```
+
+
+
